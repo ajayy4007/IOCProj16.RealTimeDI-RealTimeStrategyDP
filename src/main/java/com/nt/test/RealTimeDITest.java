1d66ca24ac6b6fe8f3e34264a7c650b81cf2023a
ajayy4007 <ajayy4007@gmail.com>
2023-06-05 12:41:08 +0530

    Create Story Bord project by using Maven

---
+++ src/main/java/com/nt/test/RealTimeDITest.java
@@ -0,0 +1,43 @@
+package com.nt.test;
+import java.util.Scanner;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import com.nt.controller.MainController;
+import com.nt.vo.CustomerVO;
+public class RealTimeDITest {
+	public static void main(String[] args) {
+		//Read input from end user as string value
+			Scanner s=new Scanner(System.in);
+			System.out.println("Enter Customer name::");
+			String name=s.nextLine();
+			System.out.println("Enter customer addrs::");
+			String addrs=s.nextLine();
+			System.out.println("Enter customer principal amount::");
+			String pAmt=s.nextLine();
+			System.out.println("Enter Time (in months)");
+			String time=s.nextLine();
+			System.out.println("Enter customer Rate of intrest::");
+			String rate=s.nextLine();
+			CustomerVO vo=new CustomerVO();
+			vo.setCustName(name);
+			vo.setCustAddrs(addrs);
+			vo.setPamt(pAmt);
+			vo.setRate(rate);
+			vo.setTime(time);
+			//create IOC container 
+			DefaultListableBeanFactory factory= new DefaultListableBeanFactory();
+			XmlBeanDefinitionReader reader=new XmlBeanDefinitionReader(factory);
+			reader.loadBeanDefinitions("com/nt/cfgs/applicationContext.xml");
+			//get controller class OBJ
+			MainController controller=factory.getBean("controller",MainController.class);
+			//invoke the methods
+			try {
+				String result=controller.processCustomer(vo);
+				System.out.println(result);
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+				System.out.println("Internal Problem...Try Again"+e.getMessage());
+			}
+}
+}
